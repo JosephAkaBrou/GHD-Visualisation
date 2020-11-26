927ac08afe32d53a79f5d65a19f33230bed6db26
JojoAkaBrou <joakabrou@gmail.com>
2020-11-26 11:50:02 +0100

    graph jo omzo

--- graph_dans_le_temps.R
+++ graph_dans_le_temps.R
@@ -1,5 +1,6 @@
 library(ggplot2)
-
+library(hrbrthemes)
+library(dplyr)
 
 data = read.csv("https://raw.githubusercontent.com/JosephAkaBrou/GHD-Visualisation/main/data3.csv")
 
@@ -26,28 +27,28 @@ location_value = location[2] # Choisi un nombre entre 1 et 45
 # L? ici on fait un filtre avec kes 3 variables au dessus. En situation r?elle, il faut une liste qui permet de choisir ? l'utilisateur
 # Quelles valeurs de sex, cause et location il veut voir. Pour l'exemple j'ai juste rendu possible la s?lection al?atoire juste haut. 
 
-filtered_df = data[(data$sex == sex_value) & (data$cause == cause_value) & (data$location == location_value),
-          c("val","year", "upper","lower")]
+filtered_df = data[(data$cause == cause_value) & (data$location == location_value),
+          c("val","year", "upper","lower", "sex")]
 
 # Ici je fais la moyenne pour chaque ann?e. Parce que apr?s avoir filtrer on obtient plusieurs valeurs par ann?e. Ces valeurs correspondent aux valeurs ####
 # des classes d'ages ####
-agg <- aggregate(list(filtered_df$val, filtered_df$upper, filtered_df$lower), by = list(filtered_df$year), mean)
-names(agg) = c("year", "val", "upper", "lower")
+agg <- aggregate(list(filtered_df$val, filtered_df$upper, filtered_df$lower), by = list(filtered_df$year, filtered_df$sex), mean)
+names(agg) = c("year", "sex", "val", "upper", "lower")
 
 
 # Et l? ici un graphique ####
 
 
 ggplot(data = agg, mapping = aes(x = year, y = val)) +
-  geom_line( colour = 'blue', size = 1, group = 1)+
-  geom_line( colour = 'blue', size = 0.8, group = 1)+
-  geom_line(data = agg, mapping = aes(x = year, y = lower) , colour = 'green', size = 1, group = 1)+
-  geom_line(data = agg, mapping = aes(x = year, y = lower) , colour = 'green', size = 0.8, group = 1)+
-  geom_line(data = agg, mapping = aes(x = year, y = upper) , colour = 'green', size = 1, group = 1)+
-  geom_line(data = agg, mapping = aes(x = year, y = upper) , colour = 'green', size = 0.8, group = 1) +
-  labs(title = 'Evolution du nombre de victime dans le temps')
-  
+  geom_line( color="grey") +
+  geom_point(shape=21, color="black", fill="#69b3a2", size=3) +
+  theme_ipsum() +
+  facet_wrap( ~ sex, ncol=3) +
+  ggtitle(paste("Evolution des morts de", cause_value,"en ", location_value))
 
+ggplot(data = agg, mapping = aes(x = year, y = val))
+
+agg
 
 
 
